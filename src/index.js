bfbd1071ed2cbc1b955d8b867c89f52bef568edc
eoliva50 <eoliva50>
2023-12-03 12:49:28 -0600

    Added basic models

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
+import bodyParser from 'body-parser'
 
 import logger from './helpers/logger'
 import router from './routes'
@@ -14,10 +15,15 @@ import { notFound, errorHandler } from './helpers/errors'
 const port = Number(process.env.PORT)
 
 const app = express()
-
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    exposedHeaders: ['x-total-count', 'x-total-pages'],
+  }),
+)
 app.use(helmet())
+app.use(bodyParser.json())
 
 app.use('/', router)
 
