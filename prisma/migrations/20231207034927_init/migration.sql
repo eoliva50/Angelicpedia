d2a5eddfb51f97d6c771b967beec13e9ca69c6a9
eoliva50 <eoliva50>
2023-12-06 22:00:20 -0600

    updated prisma

---
+++ prisma/migrations/20231207034927_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Employee" (
+    "employeeId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quote" TEXT,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("employeeId")
+);
